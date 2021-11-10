afdd6416f80e12e5f46629c6d10d0eedbc579a39
KimJongJoris <440484@student.fontys.nl>
2021-11-10 16:03:52 +0100

    Addition DbContext + Connectionstring

--- Experling-API/Experling-API/Startup.cs
+++ Experling-API/Experling-API/Startup.cs
@@ -12,6 +12,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 
+
 namespace Experling_API
 {
     public class Startup
@@ -26,6 +27,16 @@ namespace Experling_API
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
+            services.AddCors(options =>
+            {
+                options.AddPolicy("CorsPolicy", builder =>
+                {
+                    builder.AllowAnyOrigin()
+                        .AllowAnyMethod().AllowAnyHeader();
+                });
+            });
+
+           
 
             services.AddControllers();
             services.AddSwaggerGen(c =>
