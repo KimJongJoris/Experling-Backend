8e98d646636d5b7c0ef9d1515a42e7f5c61ec67e
KimJongJoris <440484@student.fontys.nl>
2021-11-17 14:02:27 +0100

    Addition of Interface + Repository

---
+++ Experling-API/Experling-API/Repository/CustomerRepository.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Experling_API.Repository
+{
+    public class CustomerRepository
+    {
+    }
+}
