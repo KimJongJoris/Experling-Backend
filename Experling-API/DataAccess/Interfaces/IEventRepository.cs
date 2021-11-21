5ed81abc25018efe801ad9a3c813060c0d87ed0d
KimJongJoris <440484@student.fontys.nl>
2021-11-21 15:27:26 +0100

    Added Interfaces for Ticket and Event

---
+++ Experling-API/DataAccess/Interfaces/IEventRepository.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using DataAccess.Models;
+
+namespace DataAccess.Interfaces
+{
+    public interface IEventRepository
+    {
+        Task<IEnumerable<EventModel>> GetEvents();
+        Task<EventModel> GetEventById();
+        Task<EventModel> AddEvent();
+        Task<EventModel> UpdateEvent();
+        Task<EventModel> DeleteEvent();
+    }
+}
---
+++ Experling-API/DataAccess/Interfaces/ITicketRepository.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Text;
+using System.Threading.Tasks;
+using DataAccess.Models;
+
+namespace DataAccess.Interfaces
+{
+    public interface ITicketRepository
+    {
+        Task<IEnumerable<TicketModel>> GetTickets();
+        Task<TicketModel> GetTicketById();
+        Task<TicketModel> AddTicket();
+        Task<TicketModel> UpdateTicket();
+        Task<TicketModel> DeleteTicket();
+    }
+}
